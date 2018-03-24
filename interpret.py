642c89802d577ccae478779e43baf36d99e62222
Matej Kňazík <mato.knazik@gmail.com>
2018-03-24 17:06:42 +0100

    istruction order loop

--- interpret.py
+++ interpret.py
@@ -1,5 +1,9 @@
 import sys
 import argparse
+import os
+import math
+import xml.etree.ElementTree as ET
+from xml.etree.ElementTree import ParseError
 
 
 def argumentsHadling():
@@ -9,14 +13,115 @@ def argumentsHadling():
     try:
         args = parser.parse_args()
     except SystemExit:
+        sys.stderr.write("ERROR 10: Wrong arguments\n")
         sys.exit(10)
 
     return args
 
 
+def openFile(fileName):
+    if os.path.exists(fileName):
+        try:
+            file = open(fileName, 'r')
+        except IOError:
+            sys.stderr.write("ERROR 11: Could not open file to read\n")
+            sys.exit(11)
+    else:
+        sys.stderr.write("ERROR 11: File not found\n")
+        sys.exit(11)
+    return file
+
+
+def parseFile(file):
+    try:
+        tree = ET.parse(file)
+    except ParseError:
+        sys.stderr.write("ERROR 31: Wrongly formatted XML file\n")
+        sys.exit(31)
+    return tree
+
+
+def checkTag(element, tag):
+    if (element.tag != tag):
+        sys.stderr.write("ERROR 31: Wrongly formatted XML file (wrong tag)\n")
+        sys.exit(31)
+    return
+
+
+def checkProgramFormatting(program):
+    checkTag(program, "program")
+    if (len(program.attrib) != 1):
+        sys.stderr.write("ERROR 31: Wrongly formatted XML file (wrong program"
+                         " attributes)\n")
+        sys.exit(31)
+    language = program.attrib.get("language")
+    if (language != "IPPcode18"):
+        sys.stderr.write("ERROR 31: Wrongly formatted XML file (unsupported"
+                         " language)\n")
+        sys.exit(31)
+    for instruction in program:
+        checkTag(instruction, "instruction")
+        if (len(instruction.attrib) != 2):
+            sys.stderr.write("ERROR 31: Wrongly formatted XML file (wrong ins"
+                             "truction attributes)\n")
+            sys.exit(31)
+        try:
+            order = int(instruction.attrib.get("order"))
+        except Exception:
+            sys.stderr.write("ERROR 31: Wrongly formatted XML file (invalid or"
+                             "der number in instruction)\n")
+            sys.exit(31)
+        if (order < 0):
+            sys.stderr.write("ERROR 31: Wrongly formatted XML file (order nu"
+                             "mber in instruction must be positive)\n")
+            sys.exit(31)
+        if (instruction.attrib.get("opcode") is None):
+            sys.stderr.write("ERROR 31: Wrongly formatted XML file (missing o"
+                             "pcode argument in instruction)\n")
+            sys.exit(31)
+
+
+def lookUpInstuct(instructionNumber, program):
+    endOfProgram = True
+    followingInstructionNum = math.inf
+    followingInstruction = None
+    for instruction in program:
+        instructOrderNum = int(instruction.attrib.get("order"))
+        if instructOrderNum == instructionNumber:
+            return instruction
+        elif instructOrderNum > instructionNumber:
+            endOfProgram = False
+            if instructOrderNum < followingInstructionNum:
+                followingInstructionNum = instructOrderNum
+                followingInstruction = instruction
+    if endOfProgram:
+        return None
+    else:
+        sys.stderr.write("WARNING: Instruction number: {} not found continu"
+                         "ing with instruction number: {}\n"
+                         .format(instructionNumber, followingInstructionNum))
+        return followingInstruction
+
+
+def interpretInstruction(instruction):
+    instructOrderNum = int(instruction.attrib.get("order"))
+    print(instruction.attrib)
+    return instructOrderNum+1
+
+
 def main():
-    args = argumentsHadling()
-    print(args.source)
+    args = argumentsHadling()  # parsing of argiments
+    fileName = ''.join(args.source)
+    file = openFile(fileName)  # open file
+    program = parseFile(file).getroot()  # get root from XML file
+
+    checkProgramFormatting(program)  # check valid tags/args of prog and instr.
+
+    instructionNumber = 1  # start with first instruction
+    instruction = lookUpInstuct(instructionNumber, program)
+    while (instruction):
+        instructionNumber = interpretInstruction(instruction)
+        instruction = lookUpInstuct(instructionNumber, program)
 
 
 if __name__ == '__main__':
